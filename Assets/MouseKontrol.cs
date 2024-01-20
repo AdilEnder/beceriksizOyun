4db4c1a11962bbd74dad55c0d7e0dda642a86891
AdilEnder <153065464+AdilEnder@users.noreply.github.com>
2024-01-20 22:11:35 +0300

    cap

---
+++ Assets/MouseKontrol.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseKontrol : MonoBehaviour
+{
+
+    public float hassasiyet;
+    public Transform govde;//karakter gövdesi
+
+    float Rotasyon = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float xR = Input.GetAxisRaw("Mouse X") * hassasiyet * Time.deltaTime;
+        float yR = Input.GetAxisRaw("Mouse Y") * hassasiyet * Time.deltaTime;
+
+        Rotasyon = Rotasyon - yR;
+        transform.localRotation = Quaternion.Euler(Rotasyon, 0f, 0f);
+
+        govde.Rotate(Vector3.up * xR);
+    }
+}
---
+++ Assets/OyuncuController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyuncuController : MonoBehaviour
+{
+
+   
+
+    void Start()
+    {
+       
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        
+    }
+}
