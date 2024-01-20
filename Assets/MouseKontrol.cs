263af19bf448cb1103b8139d63e2a831e3afd388
AdilEnder <153065464+AdilEnder@users.noreply.github.com>
2024-01-20 23:12:46 +0300

    contr

--- Assets/MouseKontrol.cs
+++ Assets/MouseKontrol.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class MouseKontrol : MonoBehaviour
 {
 
-    public float hassasiyet;
+    public float hassasiyet = 100f;
     public Transform govde;//karakter gövdesi
 
     float Rotasyon = 0f;
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     
@@ -21,7 +21,7 @@ public class MouseKontrol : MonoBehaviour
         float xR = Input.GetAxisRaw("Mouse X") * hassasiyet * Time.deltaTime;
         float yR = Input.GetAxisRaw("Mouse Y") * hassasiyet * Time.deltaTime;
 
-        Rotasyon = Rotasyon - yR;
+        Rotasyon = Mathf.Clamp(Rotasyon - yR, -90f, 90f);
         transform.localRotation = Quaternion.Euler(Rotasyon, 0f, 0f);
 
         govde.Rotate(Vector3.up * xR);
--- Assets/OyuncuController.cs
+++ Assets/OyuncuController.cs
@@ -4,17 +4,54 @@ using UnityEngine;
 
 public class OyuncuController : MonoBehaviour
 {
+    private CharacterController control;
 
-   
+    Vector3 velo;
+    public bool yerdemi;
 
+    public LayerMask maske;
+    public Transform yer;
+    public float cekimhizi = 0.3f;
+
+    public float hiz;
+    public float ziplamaYukseklik;
+    public float yercekimi;
+
+    
     void Start()
     {
-       
-        Cursor.lockState = CursorLockMode.Locked;
+        control = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        float saga = Input.GetAxis("Horizontal");
+        float ileri = Input.GetAxis("Vertical");
+
+        #region Haraket
+        Vector3 yurume = transform.right * saga + transform.forward * ileri;
+        control.Move(yurume * hiz * Time.deltaTime);
+        #endregion
+
+
+        #region Zýplama
         
+        if (Input.GetKeyDown(KeyCode.Space) && yerdemi)
+        {
+            print("zýpla");
+            velo.y = velo.y + Mathf.Sqrt(ziplamaYukseklik * -3.0f * yercekimi);
+        }
+
+        #endregion
+
+        yerdemi = Physics.CheckSphere(yer.position, cekimhizi, maske);
+        if(yerdemi && velo.y<0)
+        {
+            velo.y = 0f;
+        }
+        velo.y = yercekimi * Time.deltaTime;
+        control.Move(velo * Time.deltaTime);
     }
+
+
 }
